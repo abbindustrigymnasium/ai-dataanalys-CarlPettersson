6dec284fef3fdaf15333891c0805e0c244c89602
CarlPettersson <54657589+CarlPettersson@users.noreply.github.com>
2021-05-10 08:37:16 +0200

    Update main.cpp

--- AI_PROJEKT/AI_project/AI_project/main.cpp
+++ AI_PROJEKT/AI_project/AI_project/main.cpp
@@ -9,20 +9,20 @@ int main() {
 	
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> dis(1.0, 153251234.0);
+	std::uniform_real_distribution<> dis(1.0, 2147483648.0);
 	float number = dis(gen);
 
 	perlin::Perlin map((long)number);
 
 	Image image;
-	image.generate(1024, 1024);
+	image.generate(512, 512);
 
 	double frequency = 16;
 	double fx = image.getWidth() / frequency;
 	double fy = image.getHeight() / frequency;
 
-	for (int y = 0; y < 1024; y++) {
-		for (int x = 0; x < 1024; x++) {
+	for (int y = 0; y < 512; y++) {
+		for (int x = 0; x < 512; x++) {
 			double p = map.accumulatedNoise2D(x / fx, y / fy, 8, 2.0f, 0.25f);
 			image.setPixel(x, y, p, p, p);
 		}
